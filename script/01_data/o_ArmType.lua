feae3c57a87543d8f566a8e87f7fb31b188090d6
zpp1156 <11565252@qq.com>
2019-12-06 14:57:05 +0800

    ArmType的o表

---
+++ script/01_data/o_ArmType.lua
@@ -0,0 +1,26 @@
+--[[1006
+
+]]
+local t = {
+[==[o_ArmType]==],
+{
+	{
+		['name']=0x10060001,
+		['showname']=[==[Lite]==],
+		['str_name']=[==[Lite]==],
+		['Token']=[==[lit]==],
+	},
+	{
+		['name']=0x10060002,
+		['showname']=[==[Medium]==],
+		['str_name']=[==[Medium]==],
+		['Token']=[==[med]==],
+	},
+	{
+		['name']=0x10060003,
+		['showname']=[==[Heavy]==],
+		['str_name']=[==[Heavy]==],
+		['Token']=[==[hvy]==],
+	},
+}}
+return t
